5509a75c879ba9cad0b52879ba792cb6ac97523e
fdesimone <fdesimone@green-logic.com>
2018-11-19 06:21:34 -0500

    Revert "renamed WorkOrderState to WorkOrderStage"
    This reverts commit bdc6cab383ac752e7446ed0cbd7358cf4e6cb36f.

---
+++ DP/dbo/Tables/WorkOrderState.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[WorkOrderState]
+(
+    [WorkOrderStateID] INT NOT NULL, 
+    [WorkOrderState] NVARCHAR(100) NOT NULL, 
+    CONSTRAINT [PK_WorkOrderState] PRIMARY KEY ([WorkOrderStateID])
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_WorkOrderState_WorkOrderState] ON [dbo].[WorkOrderState] ([WorkOrderState])
