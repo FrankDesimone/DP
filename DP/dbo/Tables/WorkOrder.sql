98f7946c2d5ee0115bf87cd29cc04952679be4d1
fdesimone <fdesimone@green-logic.com>
2018-11-19 06:24:56 -0500

    renamed WorkOrderState to WorkOrderStatus

--- DP/dbo/Tables/WorkOrder.sql
+++ DP/dbo/Tables/WorkOrder.sql
@@ -2,14 +2,14 @@
 (
     [WorkOrderID] INT NOT NULL, 
     [CompanyLocationID] INT NOT NULL, 
-	[WorkOrderStateID] INT NOT NULL,
+	[WorkOrderStatusID] INT NOT NULL,
 	[VehicleID] INT NULL,
 	[EngineID] INT NULL,
 	[ECDID] INT NOT NULL,
     [DateAdded] DATETIME NOT NULL DEFAULT getdate(), 
     CONSTRAINT [PK_WorkOrder] PRIMARY KEY ([WorkOrderID]), 
     CONSTRAINT [FK_WorkOrder_CompanyLocation] FOREIGN KEY ([CompanyLocationID]) REFERENCES CompanyLocations(CompanyLocationsID), 
-    CONSTRAINT [FK_WorkOrder_WorkOrderState] FOREIGN KEY ([WorkOrderStateID]) REFERENCES [WorkOrderStatus]([WorkOrderStatusID]), 
+    CONSTRAINT [FK_WorkOrder_WorkOrderState] FOREIGN KEY ([WorkOrderStatusID]) REFERENCES [WorkOrderStatus]([WorkOrderStatusID]), 
     CONSTRAINT [FK_WorkOrder_Engine] FOREIGN KEY ([EngineID]) REFERENCES [Engine]([EngineID]), 
     CONSTRAINT [FK_WorkOrder_Vehicle] FOREIGN KEY ([VehicleID]) REFERENCES [Vehicle]([VehicleID]), 
     CONSTRAINT [FK_WorkOrder_ECD] FOREIGN KEY ([ECDID]) REFERENCES [ECD]([ECDID])
@@ -21,4 +21,4 @@ CREATE INDEX [IX_WorkOrder_CompanyLocationID] ON [dbo].[WorkOrder] ([CompanyLoca
 
 GO
 
-CREATE INDEX [IX_WorkOrder_WorkOrderStateID] ON [dbo].[WorkOrder] ([WorkOrderStateID])
+CREATE INDEX [IX_WorkOrder_WorkOrderStatusID] ON [dbo].[WorkOrder] ([WorkOrderStatusID])
