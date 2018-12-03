f42a16ecf00b7525701949f6dc9909896fdf5be2
fdesimone <fdesimone@green-logic.com>
2018-12-03 07:59:41 -0500

    refactor

--- DP/dbo/StoredProcedures/spContactsUpsert.sql
+++ DP/dbo/StoredProcedures/spContactsUpsert.sql
@@ -1,19 +1,19 @@
 ﻿CREATE PROCEDURE [dbo].[spContactsUpsert]
-		@CompanyID      INT          
-		,@ContactsID INT = NULL	
-		,@FirstName NVARCHAR (100) 
-		,@LastName NVARCHAR (100) 
-		,@Address1 NVARCHAR (100) 
-		,@Address2 NVARCHAR (100)
-		,@City     NVARCHAR (100) 
-		,@Zip     NVARCHAR (20) 
-		,@StateID		INT
-		,@Active         BIT          = 1 
-		,@Phone NVARCHAR (100) 
-		,@Email NVARCHAR (100) 
-		,@NewContactsID      INT          = NULL OUTPUT
-		,@ErrorCode as INT = 0 OUTPUT
-		,@ErrorMsg as VARCHAR(8000) = '' OUTPUT
+	@CompanyID      INT          
+	,@ContactsID INT = NULL	
+	,@FirstName NVARCHAR (100) 
+	,@LastName NVARCHAR (100) 
+	,@Address1 NVARCHAR (100) 
+	,@Address2 NVARCHAR (100)
+	,@City     NVARCHAR (100) 
+	,@Zip     NVARCHAR (20) 
+	,@StateID		INT
+	,@Active         BIT          = 1 
+	,@Phone NVARCHAR (15) 
+	,@Email NVARCHAR (100) 
+	,@NewContactsID      INT = NULL OUTPUT
+	,@ErrorCode as INT = 0 OUTPUT
+	,@ErrorMsg as VARCHAR(8000) = '' OUTPUT
 AS
 SET NOCOUNT ON;
 
@@ -27,47 +27,46 @@ BEGIN TRY
 
 	UPDATE con
 	set 
-	 con.CompanyID   = @CompanyID      
-	,con.FirstName=@FirstName 
-	,con.LastName 	=@LastName 
-    
-    ,con.Address1 =  @Address1  
-    ,con.Address2 =  @Address2  
-	,con.City     =	  @City      
-    ,con.Zip    =	  @Zip      
-	,con.StateID	= @StateID
-	,con.[Phone] = @Phone
-	,con.[Email] = @Email
-	,con.Active = @Active
+		 con.CompanyID   = @CompanyID      
+		,con.FirstName = @FirstName 
+		,con.LastName = @LastName 
+		,con.Address1 =  @Address1  
+		,con.Address2 =  @Address2  
+		,con.City     =	  @City      
+		,con.Zip    =	  @Zip      
+		,con.StateID	= @StateID
+		,con.[Phone] = @Phone
+		,con.[Email] = @Email
+		,con.Active = @Active
 	from Contacts con
 	WHERE con.ContactsID = @ContactsID;
 
 	if @@ROWCOUNT = 0
 	begin
 	INSERT INTO [dbo].[Contacts]
-           ([CompanyID]
-           ,[Address1]
-           ,[Address2]
-           ,[City]
-           ,[Zip]
-           ,[FirstName]
-           ,[LastName]
-           ,[Phone]
-           ,[Email]
-           ,[Active]
-           ,[StateID])
+        ([CompanyID]
+        ,[Address1]
+        ,[Address2]
+        ,[City]
+        ,[Zip]
+        ,[FirstName]
+        ,[LastName]
+        ,[Phone]
+        ,[Email]
+        ,[Active]
+        ,[StateID])
      VALUES
-           (@CompanyID
-           ,@Address1 
-           ,@Address2 
-           ,@City 
-           ,@Zip
-           ,@FirstName 
-           ,@LastName 
-           ,@Phone
-           ,@Email
-           ,@Active
-           ,@StateID)
+        (@CompanyID
+        ,@Address1 
+        ,@Address2 
+        ,@City 
+        ,@Zip
+        ,@FirstName 
+        ,@LastName 
+        ,@Phone
+        ,@Email
+        ,@Active
+        ,@StateID);
 	
 		SET @ContactsID	=  SCOPE_IDENTITY();
 	END
@@ -83,7 +82,7 @@ BEGIN CATCH
 	,@ErrorMessage VARCHAR(500) = ERROR_MESSAGE()
 	,@ErrorNote VARCHAR(500) = ERROR_MESSAGE();
 		
-	SELECT @ErrorMessage = '[[spContactsUpsert]] :: ' 
+	SELECT @ErrorMessage = '[spContactsUpsert] :: ' 
 			+ ERROR_PROCEDURE()
 			+ ' Line: ' + CAST(ERROR_LINE() as VARCHAR(20))
 			+  ' - ' + coalesce(@ErrorMessage , '') + ' Err #: ' + cast(ERROR_NUMBER() as varchar(8));
