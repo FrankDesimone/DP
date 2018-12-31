00b1b80372a9b705fe5ee3f3748a1e102b466685
fdesimone <fdesimone@green-logic.com>
2018-12-31 07:47:40 -0500

    QA controls

--- DP/dbo/Tables/QAAshColor.sql
+++ DP/dbo/Tables/QAAshColor.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[QAAshColor]
 (
     [QAAshColorID] INT NOT NULL, 
-    [QAAshColor] NVARCHAR(250) NOT NULL
+    [QAAshColor] NVARCHAR(250) NOT NULL, 
+    CONSTRAINT [PK_QAAshColor] PRIMARY KEY ([QAAshColorID])
 )
