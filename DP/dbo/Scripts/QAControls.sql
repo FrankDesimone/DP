0316363c4c9d4cdcc8ab5f59a6b5a21142fb4110
fdesimone <fdesimone@green-logic.com>
2018-12-31 07:55:39 -0500

    QA Controls

---
+++ DP/dbo/Scripts/QAControls.sql
@@ -0,0 +1,27 @@
+﻿insert into QAAshOnFace (QAAshOnFaceID, QAAshOnFace)
+values (0,'None')
+	,(1,'Light')
+	,(2,'Medium')
+	,(3,'Heavy');
+
+insert into QAAshColor (QAAshColorID, QAAshColor)
+values (0, 'NA');
+
+insert into QACoolant (QACoolantID, QACoolant)
+values (0, 'None')
+	,(1, 'Indication of Collant')
+	,(2, 'Red color ash');
+
+insert into QASootOnFace (QASootOnFaceID, QASootOnFace)
+values (0, 'None')
+	,(1,'Light')
+	,(2, 'Medium')
+	,(3, 'Heavy');
+
+insert into QASubstrate (QASubstrateID, QASubstrate)
+values (0, 'None')
+	,(1, '<15')
+	,(2, '15-25')
+	,(3, '25-50')
+	,(4, '50-100')
+	,(5, '>100');
--- DP/dbo/Tables/QABreach.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[QABreach]
-(
-    [QABreachID] INT NOT NULL, 
-    [QABreach] NVARCHAR(250) NOT NULL, 
-    CONSTRAINT [PK_QABreach] PRIMARY KEY ([QABreachID]) 
-)
