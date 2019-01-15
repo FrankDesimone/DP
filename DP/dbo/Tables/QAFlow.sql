8f975cc3c05b59e246ef2e764d9e445448c058be
fdesimone <fdesimone@green-logic.com>
2019-01-15 08:11:23 -0500

    QAFlow

--- DP/dbo/Tables/QAFlow.sql
+++ DP/dbo/Tables/QAFlow.sql
@@ -2,10 +2,20 @@
 (
     [QAFlowID] INT NOT NULL IDENTITY, 
     [WorkOrderID] INT NOT NULL, 
+	[TargetVelocity] FLOAT,
+	[MaxHertz] REAL,
+	[AirTemp] REAL,
+	[BarometricPressure] REAL,
+	[BackPressure] REAL,
+	[Coefficient_a] FLOAT,
+	[Coefficient_b] FLOAT,
+	[Coefficient_c] FLOAT,
+    [DateAdded] DATETIME NOT NULL DEFAULT getdate(), 
     CONSTRAINT [PK_QAFlow] PRIMARY KEY ([QAFlowID]), 
-    CONSTRAINT [FK_QAFlow_ToTable] FOREIGN KEY ([WorkOrderID]) REFERENCES [WorkOrder]([WorkOrderID])
+    CONSTRAINT [FK_QAFlow_WorkOrder] FOREIGN KEY ([WorkOrderID]) REFERENCES [WorkOrder]([WorkOrderID]) 
 )
 
 GO
+
 
 CREATE INDEX [IX_QAFlow_WorkOrderID] ON [dbo].[QAFlow] ([WorkOrderID])
---
+++ DP/dbo/Tables/QAFlowResults.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[QAFlowResults]
+(
+	[QAFlowResultsID] INT NOT NULL IDENTITY,
+	[QAFlowID] INT NOT NULL, 
+    [PSI] REAL NULL, 
+    [SpaceVelocity] FLOAT NULL, 
+    [DateAdded] DATETIME NOT NULL DEFAULT getdate(), 
+    CONSTRAINT [PK_QAFlowResults] PRIMARY KEY ([QAFlowResultsID]), 
+    CONSTRAINT [FK_QAFlowResults_QAFlow] FOREIGN KEY ([QAFlowID]) REFERENCES [QAFlow]([QAFlowID])
+)
+
+GO
+
+CREATE INDEX [IX_QAFlowDtl_QAFlowResults] ON [dbo].[QAFlowResults] ([QAFlowID])
