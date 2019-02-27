5c6a74cf16e7a403f1140cc46c9dff3c5e218e2b
fdesimone <fdesimone@green-logic.com>
2019-02-27 08:32:01 -0500

    added SalesNo

---
+++ DP/dbo/Functions/fnSalesNo.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[fnSalesNo]
+(
+	@WorkOrderID as int
+)
+RETURNS varchar(50)
+AS
+BEGIN
+	declare @SalesNo as varchar(50) = '';
+
+	select @SalesNo = c.CompanyInitials + '-' + cast(s.SalesID as varchar(12)) + '-' + cast(w.WorkOrderID as varchar(12))
+	from WorkOrder as w
+		inner join Sales as s on w.SalesID = s.SalesID
+		inner join Company as c on s.BillingCompanyID = c.CompanyID
+	where w.WorkOrderID = @WorkOrderID;
+
+	RETURN @SalesNo;
+END
