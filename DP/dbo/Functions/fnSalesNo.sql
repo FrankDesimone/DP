713fb98b0be2a8834eca01170bc392482a7de741
fdesimone <fdesimone@green-logic.com>
2019-03-10 13:36:33 -0400

    removed workorderid from sales order cleaned DPF worksheet

--- DP/dbo/Functions/fnSalesNo.sql
+++ DP/dbo/Functions/fnSalesNo.sql
@@ -7,7 +7,7 @@ AS
 BEGIN
 	declare @SalesNo as varchar(50) = '';
 
-	select @SalesNo = c.CompanyInitials + '-' + cast(s.SalesID as varchar(12)) + '-' + cast(w.WorkOrderID as varchar(12))
+	select @SalesNo = coalesce(c.CompanyInitials, '')  + '-' + cast(s.SalesID as varchar(12)) 
 	from WorkOrder as w
 		inner join Sales as s on w.SalesID = s.SalesID
 		inner join Company as c on s.BillingCompanyID = c.CompanyID
